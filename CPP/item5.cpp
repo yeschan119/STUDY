2ee65523100b72ce60e4f7ad76316d39bea2cc02
eungchan-kang <yeschan119@gmail.com>
2023-05-31 11:08:37 +0900

    comment

--- CPP/item5.cpp
+++ CPP/item5.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+// compiler가 은근 슬쩍 만드는 생성자들을 조심
+// default, copy, assignment constructor
+// 타입 변환이나 참조자, const등을 고려하지 않으므로...
 template <typename T>
 class NamedObject
 {
@@ -25,7 +27,7 @@ int main()
     NamedObject<int> p(newDog, 2);
     NamedObject<int> s(oldDog, 36);
 
-    //p = s;
+    //p = s;  // default assignement 생성자로 수행되지만 const타입을 변환시키려 하므로 에러 발생
 
     std::cout<<"end"<<std::endl;
 
