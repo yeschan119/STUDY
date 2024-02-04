0b055980aab7ea94efbf247c1f1e2abcd47834be
eungchan-kang <yeschan119@gmail.com>
2024-02-04 18:11:03 +0900

    literal type, as const 사용

---
+++ typescript/index4.js
@@ -0,0 +1,21 @@
+//literal type 지정
+//아래 변수에는 EUNGCHAN만 들어올 수 있음
+var kane;
+//kane, kang만 입력받고 1, 0만 리턴해주는 함수 만들기
+function func4(arg) {
+    return 1;
+}
+//함수 argument, return value도 literal type을 정할 수 있음
+// literal type은 문자열처럼 보이지만 실제 type이다. 즉 type이 'ROCK'
+function rps(arg) {
+    if (arg == 'PAPER') {
+        return ['PAPER'];
+    }
+    return ['PAPER'];
+}
+var DATA = {
+    name: 'ROCK',
+};
+//DATA.name은 type이 string인데 rps에서 요구하는 것은 'ROCK' .. type이므로 안됨
+//이 경우 data type에 const 붙이면 됨(readonly로 정하면서 literal type으로 사용하고 싶을 때 이거 씀)
+rps(DATA.name);
