ca299d20f2292222a79b4ed9d7038129ebf8fe37
eungchan-kang <yeschan119@gmail.com>
2024-02-16 21:58:52 +0900

    promise then, async await

---
+++ typescript/index11.ts
@@ -0,0 +1,10 @@
+function logName() {
+    console.log('timeout start')
+    
+    setTimeout(function() {
+        console.log('timeout loading...')
+    }, 2000);
+    console.log('timeout end')
+}
+
+logName()
