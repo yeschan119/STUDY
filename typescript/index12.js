6ca2f41a538aa96e2791674832a87251fdb10319
eungchan-kang <yeschan119@gmail.com>
2024-02-18 21:43:59 +0900

    empty

--- typescript/index12.js
+++ typescript/index12.js
@@ -3,3 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.name = void 0;
 exports.name = 'Kang';
 var age = 30;
+//generic 함수
+function 함수(x) {
+    return x[0];
+}
+var a = 함수([10, 5]);
+//let b = a + 1;  에러 발생함. 함수 파라미터 타입을 unknown으로 지정했기 때문
+//위와 같은 문제를 해결하기 위해 generic함수 사용(narrowing이 필요 없음)
+function 함수2(x) {
+    return x[0];
+}
+var b = 함수2([10, 5]);
+var c = b + 10;
--- typescript/index12.ts
+++ typescript/index12.ts
@@ -1,3 +1,4 @@
+//type, var export
 export type NAME = string;
 export var name = 'Kang';
 
@@ -8,3 +9,19 @@ export namespace NEW_NAME {
 }
 
 var age:NEW_NAME.NEWNAME = 30
+
+//generic 함수
+function 함수(x : unknown[]) {
+    return x[0]
+}
+
+let a = 함수([10,5])
+
+//let b = a + 1;  에러 발생함. 함수 파라미터 타입을 unknown으로 지정했기 때문
+//위와 같은 문제를 해결하기 위해 generic함수 사용(narrowing이 필요 없음)
+function 함수2<Type>(x :Type[]):Type {
+    return x[0]
+}
+
+let b = 함수2<number>([10, 5])
+let c = b + 10
